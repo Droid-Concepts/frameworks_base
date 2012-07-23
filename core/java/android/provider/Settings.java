788ba21171e380d44a9751fdf0bd9013c7f7a984
DvTonder <david.vantonder@gmail.com>
2012-07-23 17:16:04 -0400

    Framework: Add the ability to enable/disable use of Profiles
    This will remove the Profiles item from the Power menu when the user has
    selected to disable the use of System Profiles.
    
    TODO: Better handling of returning a default profile when requested
    
    Change-Id: I3c29b1bbd8f72c5432a1906a6b412d9412279c9f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3013,6 +3013,12 @@ public final class Settings {
         public static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
 
         /**
+         * Show the pending notification counts as overlays on the status bar
+         * @hide
+         */
+        public static final String SYSTEM_PROFILES_ENABLED = "system_profiles_enabled";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
