7dc67a6bb6b98d90cbfa02eba6383e0c86958014
Gergely Szell (sethyx) <szell@hu.ibm.com>
2012-11-30 21:19:36 +0100

    QuickSettings: Fix NFC and Wi-Fi Tiles
    Change-Id: I028abed74288e9d784449832860e574c8225af5d
    Signed-off-by: Gergely Szell (sethyx) <szell@hu.ibm.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -178,6 +178,7 @@ class QuickSettings {
     private BluetoothState mBluetoothState;
     private TelephonyManager tm;
     private ConnectivityManager mConnService;
+    private NfcAdapter mNfcAdapter;
 
     private AokpTarget mAokpTarget;
 
@@ -311,15 +312,16 @@ class QuickSettings {
         updateWifiDisplayStatus();
         updateResources();
 
-        if (getCustomUserTiles().contains(SIGNAL_TOGGLE))
+        ArrayList<String> userTiles = getCustomUserTiles();
+        if (userTiles.contains(SIGNAL_TOGGLE) || userTiles.contains(WIFI_TOGGLE))
             networkController.addNetworkSignalChangedCallback(mModel);
-        if (getCustomUserTiles().contains(BLUETOOTH_TOGGLE))
+        if (userTiles.contains(BLUETOOTH_TOGGLE))
             bluetoothController.addStateChangedCallback(mModel);
-        if (getCustomUserTiles().contains(BATTERY_TOGGLE))
+        if (userTiles.contains(BATTERY_TOGGLE))
             batteryController.addStateChangedCallback(mModel);
-        if (getCustomUserTiles().contains(GPS_TOGGLE))
+        if (userTiles.contains(GPS_TOGGLE))
             locationController.addStateChangedCallback(mModel);
-        if (getCustomUserTiles().contains(ROTATE_TOGGLE))
+        if (userTiles.contains(ROTATE_TOGGLE))
             RotationPolicy.registerRotationPolicyListener(mContext, mRotationPolicyListener,
                     UserHandle.USER_ALL);
     }
@@ -600,7 +602,7 @@ class QuickSettings {
                 quick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        wifiManager.setWifiEnabled(wifiManager.isWifiEnabled() ? false : true);
+                        wifiManager.setWifiEnabled(!wifiManager.isWifiEnabled());
                     }
                 });
                 quick.setOnLongClickListener(new View.OnLongClickListener() {
@@ -912,17 +914,17 @@ class QuickSettings {
                 quick.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        NfcAdapter mNfcAdapter = NfcAdapter.getDefaultAdapter(mContext);
                         boolean enabled = false;
-                        if (mNfcAdapter != null) {
-                            enabled = mNfcAdapter.isEnabled();
+                        if (mNfcAdapter == null) {
+                            mNfcAdapter = NfcAdapter.getDefaultAdapter();
+                            mModel.setNfcAdapter(mNfcAdapter);
                         }
+                        enabled = mNfcAdapter.isEnabled();
                         if (enabled) {
                             mNfcAdapter.disable();
                         } else {
                             mNfcAdapter.enable();
                         }
-                        mHandler.postDelayed(delayedRefresh, 1000);  
                     }
                 });
                 quick.setOnLongClickListener(new View.OnLongClickListener() {
@@ -1530,7 +1532,6 @@ class QuickSettings {
         public void run() {
             mModel.refreshWifiTetherTile();
             mModel.refreshUSBTetherTile();
-            mModel.refreshNFCTile();
             mModel.refreshTorchTile();
         }
     };
