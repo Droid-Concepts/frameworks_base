e942c517e51c733a1a7b77139b3e7a9c50e2c4cc
Roman Birg <romanbirg@gmail.com>
2013-01-03 00:52:29 -0700

    Framework: NavBarColor (1/2)
    Change-Id: I7ed579d95d921e08d8f9d0a9160605457b8c4cb3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3860,6 +3860,13 @@ public final class Settings {
          */
         public static final String NAVIGATION_BAR_ALPHA = "navigation_bar_alpha";
 
+        /**
+         * Navigation bar color.
+         *
+         * @hide
+         */
+        public static final String NAVIGATION_BAR_COLOR = "navigation_bar_color";
+
     }
 
     /**
