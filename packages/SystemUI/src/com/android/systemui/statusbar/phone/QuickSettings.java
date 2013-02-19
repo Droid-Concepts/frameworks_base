8a6a3e4ec69a200dfd40dd9a5aea38d6ba28ff10
kecinzer <kecinzer@gmail.com>
2013-02-19 13:28:06 +0100

    Fix longpress action for tehter toggles
    Jumps directly into tether settings.
    
    Change-Id: Ifdc825bb3f816e60d6e9232501935a26f757128c
    Signed-off-by: kecinzer <kecinzer@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
@@ -963,7 +963,9 @@ class QuickSettings {
                 quick.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
-                        startSettingsActivity(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName("com.android.settings", "com.android.settings.Settings$TetherSettingsActivity"));
+                        startSettingsActivity(intent);
                         return true;
                     }
                 });
@@ -993,7 +995,9 @@ class QuickSettings {
                 quick.setOnLongClickListener(new View.OnLongClickListener() {
                     @Override
                     public boolean onLongClick(View v) {
-                        startSettingsActivity(android.provider.Settings.ACTION_WIRELESS_SETTINGS);
+                        Intent intent = new Intent();
+                        intent.setComponent(new ComponentName("com.android.settings", "com.android.settings.Settings$TetherSettingsActivity"));
+                        startSettingsActivity(intent);
                         return true;
                     }
                 });
