06c7497dd7f80c173c08912078c195cf3d015a11
bmc08gt <bmc08gt.teamb00sted@gmail.com>
2013-04-17 04:24:12 +0200

    SystemUI: StayAwake Toggle
    Instead of overcomplicating toggle, onLongClick simply takes the user to
    Settings$DisplaySettingsActivity instead. onClick (on) sets screen timeout to
    int MAX_VALUE(approx 24 days), and restores user value onClick (off).
    
    ** Icon Credit to Nitroz(Thanks a ton!)
    
    Change-Id: Ifd2051b2a6e8235160713cad3b4385dbb6d66b8f
    Signed-off-by: bmc08gt <bmc08gt.teamb00sted@gmail.com>
    
    Conflicts:
    	packages/SystemUI/res/values/strings.xml

--- packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/toggles/ToggleManager.java
@@ -96,6 +96,7 @@ public class ToggleManager {
     public static final String STATUSBAR_TOGGLE = "STATUSBAR";
     public static final String SCREENSHOT_TOGGLE = "SCREENSHOT";
     public static final String CUSTOM_TOGGLE = "CUSTOM";
+    public static final String STAYAWAKE_TOGGLE = "STAYAWAKE";
 
     private int mStyle;
 
@@ -162,6 +163,7 @@ public class ToggleManager {
             toggleMap.put(STATUSBAR_TOGGLE, StatusbarToggle.class);
             toggleMap.put(SCREENSHOT_TOGGLE, ScreenshotToggle.class);
             toggleMap.put(CUSTOM_TOGGLE, CustomToggle.class);
+            toggleMap.put(STAYAWAKE_TOGGLE, StayAwakeToggle.class);
             // toggleMap.put(BT_TETHER_TOGGLE, null);
         }
         return toggleMap;
