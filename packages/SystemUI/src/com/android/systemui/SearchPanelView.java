e58d779a55d36f4f1d48a0d423e18414857d74c2
Jubakuba <Jubakuba@gmail.com>
2013-06-12 05:08:57 -0600

    NavRing: Longpress Logic
    Again, this matches shortpress logic.
        (And now matches longpress logic
        since this was merged on Lockscreen)
    
    Vibrates on Longpress...
        Remove finger to launch.
    
    Change-Id: Ia7f8bb2580643930511fd12764f4a41873ef64d2
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/SearchPanelView.java

--- packages/SystemUI/src/com/android/systemui/SearchPanelView.java
+++ packages/SystemUI/src/com/android/systemui/SearchPanelView.java
@@ -211,15 +211,10 @@ public class SearchPanelView extends FrameLayout implements
 
        final Runnable SetLongPress = new Runnable () {
             public void run() {
-                if (!mSearchPanelLock) {
-                    mSearchPanelLock = true;
+                if (!mLongPress) {
+                    vibrate();
                     mLongPress = true;
-                    if (shouldUnlock(longList.get(mTarget))) {
-                        maybeSkipKeyguard();
-                    }
-                    AwesomeAction.launchAction(mContext, longList.get(mTarget));
-                    mBar.hideSearchPanel();
-                 }
+                }
             }
         };
 
@@ -228,6 +223,14 @@ public class SearchPanelView extends FrameLayout implements
         }
 
         public void onReleased(View v, int handle) {
+            if (!mSearchPanelLock && mLongPress) {
+                mSearchPanelLock = true;
+                if (shouldUnlock(longList.get(mTarget))) {
+                    maybeSkipKeyguard();
+                }
+                AwesomeAction.launchAction(mContext, longList.get(mTarget));
+                mBar.hideSearchPanel();
+            }
         }
 
         public void onTargetChange(View v, final int target) {
@@ -612,12 +615,8 @@ public class SearchPanelView extends FrameLayout implements
                 resolver.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.SYSTEMUI_NAVRING_ICON[i]), false, this);
             }
-
-<<<<<<< HEAD
         }
 
-=======
->>>>>>> ac82ed9... SystemUI: Fix blank Navring on boot,
         @Override
         public void onChange(boolean selfChange) {
             updateSettings();
