ee2294257d912fc4a45348f513465338d09ff641
cphelps76 <cphelps76@gmail.com>
2013-08-27 09:24:16 -0500

    Revert "Frameworks: privacy guard fix possible NPE"
    This reverts commit ccc4151459230183b7ff7e230020c733bc952a58.

--- services/java/com/android/server/pm/Settings.java
+++ services/java/com/android/server/pm/Settings.java
@@ -451,13 +451,10 @@ final class Settings {
                             final boolean installed = installUser == null
                                     || installUser.getIdentifier() == UserHandle.USER_ALL
                                     || installUser.getIdentifier() == user.id;
-                            boolean privacyGuard = false;
-                            if (installUser != null) {
-                                privacyGuard = android.provider.Settings.Secure.getIntForUser(
+                            final boolean privacyGuard = android.provider.Settings.Secure.getIntForUser(
                                     mContext.getContentResolver(),
                                     android.provider.Settings.Secure.PRIVACY_GUARD_DEFAULT,
                                     0, user.id) == 1;
-                            }
                             p.setUserState(user.id, COMPONENT_ENABLED_STATE_DEFAULT,
                                     installed,
                                     true, // stopped,
