3f96f54a464191d4b5a5209f1c3d44a9f2d23d81
cphelps76 <cphelps76@gmail.com>
2013-08-28 23:50:03 -0500

    Set AwesomeAction.java torch intent to toggle cm torch

--- packages/SystemUI/src/com/android/systemui/aokp/AwesomeAction.java
+++ packages/SystemUI/src/com/android/systemui/aokp/AwesomeAction.java
@@ -169,11 +169,7 @@ public class AwesomeAction {
                 mContext.sendBroadcast(new Intent("android.settings.SHOW_INPUT_METHOD_PICKER"));
                 break;
             case ACTION_TORCH:
-                Intent intentTorch = new Intent("android.intent.action.MAIN");
-                intentTorch.setComponent(ComponentName.unflattenFromString("com.aokp.Torch/.TorchActivity"));
-                intentTorch.addCategory("android.intent.category.LAUNCHER");
-                intentTorch.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mContext.startActivity(intentTorch);
+                mcontext.sendBroadcast(new Intent("net.cactii.flash2.TOGGLE_FLASHLIGHT"));
                 break;
             case ACTION_TODAY:
                 long startMillis = System.currentTimeMillis();
