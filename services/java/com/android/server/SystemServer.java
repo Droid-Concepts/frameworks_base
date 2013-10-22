723b8de3909c817f350eee87eba807d4ee5794c4
davros <davros@droidconcepts.com>
2013-10-21 20:26:56 -0400

    Revert "Support GESTURE_SENSOR input device type with GestureService"
    This reverts commit d49ad3ef1b0662f5dc2aa7e101cb927834d00d3f.

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -55,7 +55,6 @@ import com.android.server.am.BatteryStatsService;
 import com.android.server.content.ContentService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.dreams.DreamManagerService;
-import com.android.server.gesture.GestureService;
 import com.android.server.input.InputManagerService;
 import com.android.server.net.NetworkPolicyManagerService;
 import com.android.server.net.NetworkStatsService;
@@ -365,7 +364,6 @@ class ServerThread extends Thread {
         TextServicesManagerService tsms = null;
         LockSettingsService lockSettings = null;
         DreamManagerService dreamy = null;
-        GestureService gestureService = null;
 
         // Bring up services needed for UI.
         if (factoryTest != SystemServer.FACTORY_TEST_LOW_LEVEL) {
@@ -802,17 +800,7 @@ class ServerThread extends Thread {
                     Slog.e(TAG, "Failure starting Irda Service", e);
                 }
             }
-
-            if (context.getResources().getBoolean(
-                    com.android.internal.R.bool.config_enableGestureService)) {
-                try {
-                    Slog.i(TAG, "Gesture Sensor Service");
-                    gestureService = new GestureService(context, inputManager);
-                    ServiceManager.addService("gesture", gestureService);
-                } catch (Throwable e) {
-                    Slog.e(TAG, "Failure starting Gesture Sensor Service", e);
-                }
-            }
+        }
 
         // Before things start rolling, be sure we have decided whether
         // we are in safe mode.
@@ -895,13 +883,6 @@ class ServerThread extends Thread {
             reportWtf("making Display Manager Service ready", e);
         }
 
-        if (gestureService != null) {
-            try {
-                gestureService.systemReady();
-            } catch (Throwable e) {
-                reportWtf("making Gesture Sensor Service ready", e);
-            }
-        }
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_APP_LAUNCH_FAILURE);
