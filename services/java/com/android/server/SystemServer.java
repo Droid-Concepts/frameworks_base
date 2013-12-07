b32598917e0ee28003de423e51e08929b3e051c4
cphelps76 <cphelps76@gmail.com>
2013-12-06 21:30:16 -0600

    Merge tag 'android-4.4.1_r1' into kk4.4
    Android 4.4.1 Release 1
    
    Conflicts:
    	packages/Keyguard/src/com/android/keyguard/CarrierText.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
    	packages/Keyguard/src/com/android/keyguard/KeyguardTransportControlView.java
    	packages/SystemUI/AndroidManifest.xml
    	packages/SystemUI/res/drawable-hdpi/search_light.png
    	packages/SystemUI/res/drawable-xhdpi/search_light.png
    	packages/SystemUI/res/drawable-xxhdpi/ic_qs_remote_display.png
    	packages/SystemUI/res/layout/status_bar.xml
    	packages/SystemUI/res/values/strings.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/CommandQueue.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/BarTransitions.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarTransitions.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettings.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsModel.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/QuickSettingsTileView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/policy/KeyButtonView.java
    	policy/src/com/android/internal/policy/impl/PhoneWindow.java
    	policy/src/com/android/internal/policy/impl/keyguard/KeyguardServiceWrapper.java
    	services/java/com/android/server/DevicePolicyManagerService.java
    	services/java/com/android/server/SystemServer.java
    	services/java/com/android/server/am/ActivityManagerService.java
    	services/java/com/android/server/am/ActivityStack.java
    	services/java/com/android/server/power/DisplayPowerController.java
    	tools/layoutlib/bridge/src/com/android/layoutlib/bridge/impl/RenderAction.java
    	tools/layoutlib/create/src/com/android/tools/layoutlib/create/Main.java
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar.xml

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -57,6 +57,7 @@ import com.android.server.content.ContentService;
 import com.android.server.display.DisplayManagerService;
 import com.android.server.dreams.DreamManagerService;
 import com.android.server.input.InputManagerService;
+import com.android.server.media.MediaRouterService;
 import com.android.server.net.NetworkPolicyManagerService;
 import com.android.server.net.NetworkStatsService;
 import com.android.server.os.SchedulingPolicyService;
@@ -362,6 +363,7 @@ class ServerThread {
         DreamManagerService dreamy = null;
         AssetAtlasService atlas = null;
         PrintManagerService printManager = null;
+        MediaRouterService mediaRouter = null;
 
         // Bring up services needed for UI.
         if (factoryTest != SystemServer.FACTORY_TEST_LOW_LEVEL) {
@@ -827,6 +829,16 @@ class ServerThread {
             } catch (Throwable e) {
                 Slog.e(TAG, "Failure starting AssetRedirectionManager Service", e);
             }
+
+            if (!disableNonCoreServices) {
+                try {
+                    Slog.i(TAG, "Media Router Service");
+                    mediaRouter = new MediaRouterService(context);
+                    ServiceManager.addService(Context.MEDIA_ROUTER_SERVICE, mediaRouter);
+                } catch (Throwable e) {
+                    reportWtf("starting MediaRouterService", e);
+                }
+            }
         }
 
         // Before things start rolling, be sure we have decided whether
@@ -949,6 +961,7 @@ class ServerThread {
         final InputManagerService inputManagerF = inputManager;
         final TelephonyRegistry telephonyRegistryF = telephonyRegistry;
         final PrintManagerService printManagerF = printManager;
+        final MediaRouterService mediaRouterF = mediaRouter;
 
         // We now tell the activity manager it is okay to run third party
         // code.  It will call back into us once it has gotten to the state
@@ -1100,6 +1113,12 @@ class ServerThread {
                     if (printManagerF != null) printManagerF.systemRuning();
                 } catch (Throwable e) {
                     reportWtf("Notifying PrintManagerService running", e);
+                }
+
+                try {
+                    if (mediaRouterF != null) mediaRouterF.systemRunning();
+                } catch (Throwable e) {
+                    reportWtf("Notifying MediaRouterService running", e);
                 }
             }
         });
